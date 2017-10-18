33e3db1264b235519e421efeee5a561ccd5d8489
guomeijie <18203212911@163.com>
2017-10-18 09:39:59 +0800

    loginComponent

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,21 +1,210 @@
+
 <template>
-  <div class="login">
-    login
-  </div>
+	<div class="songguo-mine">
+		<div class="login-mountain">
+			
+			<img src="../images/navLogo.png" class="login"/>
+			
+		   	<div class="login-box">
+			   	<div class="weibo-login-button">
+		            <img src="../images/login_weibo.png">
+		            <span>微博账号登录</span>
+		        </div>
+		        <div class="wechat-login-button">
+		            <img src="../images/login_wechat.png">
+		            <span>微博账号登录</span>
+		        </div>
+	        </div>
+	        
+	        <p class="others">或者</p>
+	        
+	        <form @submit.prevent="handlelogin">
+	        	<input type="text" placeholder="手机号" class="phone inp" v-model="username"/>
+	        	<input type="text" placeholder="密码" class="pwd inp" v-model="password"/>
+	        	<p class="forgot-pwd">
+	        		<a href="#" class="forgot-pwd-a">忘记密码></a>	
+	        	</p>
+	        	<button type="submit" class="login-btn">登录</button>
+	        </form>
+	        
+	        <div class="register-box">
+	        	<div class="install-app">
+				    没有账号，下载应用即可注册
+				</div>
+				<div class="go-to-install">
+					<a><img src="../images/appstore.png"></a>
+					<a><img src="../images/googleplay.png"></a>
+				</div>
+	        </div>
+	        
+	    </div>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'Login',
-  data () {
-    return {
-     
-    }
-  }
-}
+	
+	import {mapActions} from "vuex"
+	
+	export default {
+	  	name: 'SongguoMine',
+	  	data () {
+		    return {
+		    	username:"",
+		    	password:""
+		    }
+		},
+		methods:{
+			...mapActions(["login"]),
+			handlelogin(){
+				alert(1)
+				this.login({username:this.username,password:this.password})
+			}
+		}
+	}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  
+	.songguo-mine{
+		height:100%;
+		width: 100%;
+	    background-image: radial-gradient(circle,#fff7f5,#f8f1e1);
+	    .login-mountain{
+	    	background:url(../images/shouping_bottom.png) no-repeat center bottom;
+	    	height:100%;
+	    	width:100%;
+	   		padding-top:0.6rem;
+	   		.login{
+	   			margin:0 auto;
+	    		width: 0.98rem;
+				height: 0.44rem;
+				display: block;
+				
+	    	}
+		    .login-box{
+		    	margin-top:0.5rem;
+		    	width:100%;
+		    	text-align: center;
+			    border: none;
+			    border-radius: none;
+			    outline: 0;
+			    box-shadow: none;
+		    	.weibo-login-button{
+		    		display: inline-block;
+				    padding: 0 0.08rem;
+				    width: 1.4rem;
+				    height: 0.3rem;
+				    border-radius: 0.15rem;
+				    border: 1px solid #ff5722;
+				    span {
+					    margin-left: 0;
+					    color: #ff5722;
+					    font-size: 12px;
+					}
+				    img{
+				    	vertical-align: middle;
+				    	width: 0.3rem;
+	    				height: 0.3rem;
+				    }
+		    	}
+		    	.wechat-login-button{
+		    		display: inline-block;
+				    padding: 0 0.08rem;
+				    width: 1.4rem;
+				    height: 0.3rem;
+				    border-radius: 0.15rem;
+				    border: 1px solid #ff5722;
+				    span {
+					    margin-left: 0;
+					    color: #ff5722;
+					    font-size: 12px;
+					}
+				    img{
+				    	vertical-align: middle;
+				    	width: 0.3rem;
+	    				height: 0.3rem;
+				    }
+		    	}
+		    }
+		    .others{
+		    	width:0.5rem;
+		    	margin:0 auto;
+		    	text-align: center;
+		    	padding: 0.1rem 0;
+				font-size: 9px;
+				color: #bdbdbd;
+		    }
+		    .register-box{
+		    	width:100%;
+		    	height:3.17rem;
+		    }
+		    form{
+		    	display: block;
+		    	width:100%;
+		    	.inp{
+		    		display: block;
+		    		background-color: #fff;
+    				color: #666;
+    				border: 1px solid #ded8d0;
+				    border-radius: 2px;
+				    width: 2.91rem;
+				    height: 0.31rem;
+				    padding: 0.06rem;
+				    font-size: 12px;
+				    margin:0 auto;
+		    	}
+		    	.pwd{
+		    		margin-top:0.1rem;
+		    	}
+		    	.forgot-pwd{
+		    		width: 2.91rem;
+		    		text-align: right;
+		    		margin:0 auto;
+		    		margin-top:0.1rem;
+		    		margin-bottom:0.3rem;
+				    font-size: 0.1rem;
+				    .forgot-pwd-a{
+				  		color: #ff5722;
+				    }
+		    	}
+		    	.login-btn{
+		    		display: block;
+		    		border: none;
+		    		width: 2.9rem;
+    				height: 0.36rem;
+				    background-color: #ff5722;
+				    border-radius: 0.02rem;
+				    color: #fff;
+				    font-size: 16px;
+				    text-align: center;
+				    line-height: 0.36rem;
+				    margin:0 auto;
+		    	}
+		    }
+		    .register-box{
+		    	width:100%;
+		    	.install-app{
+		    		width:3rem;
+		    		text-align: center;
+		    		margin:0 auto;
+		    		margin-top: 0.7rem;
+				    margin-bottom: 0.1rem;
+				    color: #666;
+				    font-size: 14px;
+		    	}
+		    	.go-to-install{
+		    		display:flex;
+		    		width: 2.91rem;
+		    		margin:0 auto;
+		    		justify-content: space-between;
+		    		a{
+		    			display:block;
+		    			img{
+		    				width: 1.35rem;
+    						height: 0.4rem;
+		    			}
+		    		}
+		    	}
+		    }
+	    }
+	}
 </style>
---
+++ src/components/login/WeiboLogin.vue
@@ -0,0 +1,121 @@
+<template>
+	<div class="weibo-login">
+		
+		<div class="line"></div>
+		
+		<div class="logo">
+			<img src="../images/logo.png" />
+			<button type="button" class="register">注册</button>
+		</div>
+		
+		<form @submit.prevent="submit" class="login-box">
+			<div class="useWeibo">
+               	使用你的微博帐号访问 <span> 松果_生活</span>
+            </div>
+            <div class="inputs">
+	            <input type="text" placeholder="请用微博账号登录"/>
+	            <input type="password" placeholder="请输入密码"/>
+	            <button type="submit">登录</button>
+            </div>
+		</form>
+	
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"WeiboLogin",
+		data(){
+			return{
+				
+			}
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.weibo-login{
+		background: url(../images/btm.png) repeat-x center bottom,url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgeDE9IjAlIiB5MT0iMCUiIHgyPSIwJSIgeTI9IjEwMCUiPgogICAgPHN0b3Agb2Zmc2V0PSIwJSIgc3RvcC1jb2xvcj0iI2ZiZmJmYiIgc3RvcC1vcGFjaXR5PSIxIi8+CiAgICA8c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiNkNGQ5ZGMiIHN0b3Atb3BhY2l0eT0iMSIvPgogIDwvbGluZWFyR3JhZGllbnQ+CiAgPHJlY3QgeD0iMCIgeT0iMCIgd2lkdGg9IjEiIGhlaWdodD0iMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
+		height:100%;
+		width:100%;
+		.line{
+			width:100%;
+			height:0.05rem;
+			background: -webkit-gradient(linear, left top, right top, from(#eb192d), to(#fdb940)) no-repeat;
+		}
+		.logo{
+			padding:0.2rem 0;
+			justify-content: space-around;
+			display:flex;
+			img{
+				width: 1.17rem;
+	    		height: 0.35rem;
+			}
+			.register{
+				border:none;
+				padding:0 0.15rem;
+				display: block;
+				border: 1px solid #bebebe;
+				border-bottom-left-radius: none;
+			    height: 0.28rem;
+			    box-shadow: inset 0 1px 0 #fff;
+			    color: #333;
+			    font-style: normal;
+			    text-shadow: 0 1px 0 #fff;
+			    display: block;
+			    height: 28px;
+			    border-radius: 1px;
+			    background: linear-gradient(top, #f9f9f9,#dbdbdb);
+			    box-shadow: 0 1px 0 rgba(0,0,0,0.1);
+			    text-align: center;
+			    line-height: 0.28rem;
+			}
+		}
+		.login-box{
+			margin:0 auto;
+			width:3rem;
+			height:2.04rem;
+			background:#FFFFFF;
+			border-radius: 0.1rem;
+			.useWeibo{
+				font-size: 14px;
+    			padding: 0.2rem 0.2rem 0.1rem;
+    			color: #333;
+			}
+			.inputs{
+				width:2.6rem;
+				margin:0 auto;
+				input{
+				    border: 1px solid #bebebe;
+				    border-radius: 0.03rem;
+				    width: 2.6rem;
+				    line-height: 0.33rem;
+				    height: 0.33rem;
+				    margin: 0 0 0.1rem;
+				    padding: 0 0.1rem;
+				    box-shadow: inset 1px 1px 2px 0 rgba(0,0,0,0.1);
+				    font-size: 18px;
+				    -webkit-box-sizing: border-box;
+				    -moz-box-sizing: border-box;
+				    box-sizing: border-box;
+				    line-height: 0.19rem;
+				}
+				button{
+					display: block;
+					width:2.6rem;
+					height:0.40rem;
+					border: 1px solid #eb192d;
+				    border-bottom-color: #be1e32;
+				    background: -webkit-gradient(linear, left top, left bottom, from(#ff555f), to(#eb192d));
+				    color: #fff;
+				    font-size: 18px;
+				    box-shadow: 0 1px 1px rgba(0,0,0,0.2), inset 0 1px 1px rgba(255,255,255,0.2);
+				    text-shadow: 0 1px 1px rgba(255,255,255,0.2);
+				}
+			}
+		}
+	}
+</style>
